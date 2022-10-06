df606f572389889a1acbfcb7021df0f37e1ec916
owethusotomela <owethusotomela.owethusotomela@gmail.com>
2022-10-06 08:14:42 +0200

    New file

---
+++ app.js
@@ -0,0 +1,19 @@
+var items = [
+    {
+        name: 'Bike', price: 12000
+    },
+    {
+        name: 'TV', price: 5000
+    },
+    {
+        name: 'Car', price: 100000
+    },
+    {
+        name: 'Album', price: 200
+    },
+    {
+        name: 'Blanket', price: 300
+    },
+]
+
+console.log(items)
